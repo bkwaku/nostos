b5e2df2d1fa738a3a5879fd52c51d595ff72cf6d
Kwaku Boateng <kbboateng14@gmail.com>
2026-01-31 12:45:38 -0500

    remove broken code

--- go_ingress/main.go
+++ go_ingress/main.go
@@ -1,34 +1,14 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"log"
 	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
 )
 
 func main() {
 	config := LoadConfig()
 	httpServer := initilizeServer(config)
-
-	// set up showdown gracefully
-	go func() {
-		sigCh := make(chan os.Signal, 1)
-		signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
-		<-sigCh
-		log.Println("[ingress] shutting down...")
-
-		ctxTimeout, cancelTimeout := context.WithTimeout(context.Background(), 10*time.Second)
-		defer cancelTimeout()
-
-		if err := httpServer.Shutdown(ctxTimeout); err != nil {
-			log.Printf("[ingress] error shutting down server: %v\n", err)
-		}
-	}()
 
 	// Start the server
 	fmt.Printf("[ingress] listening on %s\n", config.ServerAddr)
